86009153267a9bcb2fe57899b1bd27466aa9679b
Samuel Trangmar-Keates <sam@message.co.uk>
2014-11-28 11:11:52 +0000

    report forms

--- src/Event/ReportEvent.php
+++ src/Event/ReportEvent.php
@@ -4,10 +4,17 @@ namespace Message\Mothership\Report\Event;
 
 use Message\Cog\Event\Event;
 use Message\Cog\DB\QueryBuilder;
+use Message\Mothership\Report\Filter\Collection as FilterCollection;
 
 class ReportEvent extends Event
 {
 	private $_queryBuilders = [];
+	private $_filters;
+
+	public function __construct()
+	{
+		$this->_filters = new FilterCollection;
+	}
 
 	public function getQueryBuilders()
 	{
@@ -19,4 +26,15 @@ class ReportEvent extends Event
 		$this->_queryBuilders[] = $queryBuilder;
 	}
 
+	public function setFilters(FilterCollection $filters)
+	{
+		$this->_filters = $filters;
+
+		return $this;
+	}
+
+	public function getFilters()
+	{
+		return $this->_filters;
+	}
 }
